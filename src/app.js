708ddf5e0af33d7a42df9b0eeab68fdd4834e676
Syllinger <syllinger@gmail.com>
2017-10-21 19:19:06 -0400

    Add data persistence using localStorage, and some additional edge case handling

--- src/app.js
+++ src/app.js
@@ -35,6 +35,30 @@ class IndecisionApp extends React.Component {
     }
   }
 
+  componentDidMount() {
+    try {
+      const json = localStorage.getItem('options');
+      const options = JSON.parse(json);
+      if (options) {
+        this.setState(() => ({options}));
+      }
+    } catch(e) {
+
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.options.length !== this.state.options.length) {
+      const json = JSON.stringify(this.state.options);
+      localStorage.setItem('options', json);
+      console.log('Saving data.');
+    }
+  }
+
+  componentWillUnmount() {
+    console.log('Component will unmount.');
+  }
+
   handleDeleteOption(optionToRemove) {
     this.setState((prevState) => ({
       options: prevState.options.filter(option => optionToRemove !== option)
@@ -128,6 +152,7 @@ const Options = (props) => {
   return (
     <div>
       <button onClick={props.handleDeleteOptions}>Remove All</button>
+      {props.options.length === 0 && <p>Please add an option to get started.</p>}
       {
         /*
         PROBLEM: We want to break JSX into multiple lines, but retain cleanliness
@@ -193,9 +218,12 @@ class AddOption extends React.Component {
     
     const option = e.target.elements.option.value.trim();
     const error = this.props.handleAddOption(option);
-    e.target.elements.option.value = '';
 
     this.setState(() => ({error}));
+    
+    if (!error) {
+      e.target.elements.option.value = '';
+    }
   }
 
   render() {
