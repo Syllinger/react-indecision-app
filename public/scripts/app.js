3d218194936e5e874c0a474e6508d6515b610890
Syllinger <syllinger@gmail.com>
2017-10-21 21:01:36 -0400

    Install babel and live-serve locally and create associated scripts

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,32 +8,55 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var Counter = function (_React$Component) {
-  _inherits(Counter, _React$Component);
+var IndecisionApp = function (_React$Component) {
+  _inherits(IndecisionApp, _React$Component);
 
-  function Counter(props) {
-    _classCallCheck(this, Counter);
+  function IndecisionApp(props) {
+    _classCallCheck(this, IndecisionApp);
 
-    var _this = _possibleConstructorReturn(this, (Counter.__proto__ || Object.getPrototypeOf(Counter)).call(this, props));
+    /*
+    PROBLEM: Methods are bound to class instances. When the function reference is passed
+    down via props, its lexical scope changes, and it runs in the context of the component 
+    that called it in its "onClick" handler. As a result, the "this" value runs in a different 
+    context than originally intended. In the handleDeleteOptions method, we reference try to
+    log "this.props.options" but "this" is not bound in this context, and we receive an error 
+    indicating that we cannot access the property "props" of undefined.
+      SOLUTION: Chain the "bind" method onto the function call in the "onClick" handler. The 
+    .bind() method, when it executes at runtime, allows "this" to be manually bound. Because 
+    "this" is bound correctly in the "render" method, we can bind the correct "this" by passing 
+    it as the argument to .bind().
+      ADD'L INFO: When we add .bind() onto the function call in the "onClick" handler, it may 
+    become expensive, as "this" will be rebound every time the component re-renders. To avoid 
+    this otherwise expensive process, we call it only once on the highest component possible in 
+    the hierarchy. Future function calls will simply access the bound function already in memory.
+    */
+    var _this = _possibleConstructorReturn(this, (IndecisionApp.__proto__ || Object.getPrototypeOf(IndecisionApp)).call(this, props));
+    /*
+    BOILERPLATE: We call super(props) to ensure that we don't lose internal 
+    "props" references created by React when the class is instantiated.
+    */
 
-    _this.handleAddOne = _this.handleAddOne.bind(_this);
-    _this.handleMinusOne = _this.handleMinusOne.bind(_this);
-    _this.handleReset = _this.handleReset.bind(_this);
+
+    _this.handleDeleteOptions = _this.handleDeleteOptions.bind(_this);
+    _this.handlePick = _this.handlePick.bind(_this);
+    _this.handleAddOption = _this.handleAddOption.bind(_this);
+    _this.handleDeleteOption = _this.handleDeleteOption.bind(_this);
 
     _this.state = {
-      count: 0
+      options: []
     };
     return _this;
   }
 
-  _createClass(Counter, [{
+  _createClass(IndecisionApp, [{
     key: 'componentDidMount',
     value: function componentDidMount() {
       try {
-        var count = localStorage.getItem('count');
-        if (count && !isNaN(count)) {
+        var json = localStorage.getItem('options');
+        var options = JSON.parse(json);
+        if (options) {
           this.setState(function () {
-            return { count: parseInt(count, 10) };
+            return { options: options };
           });
         }
       } catch (e) {}
@@ -41,45 +64,222 @@ var Counter = function (_React$Component) {
   }, {
     key: 'componentDidUpdate',
     value: function componentDidUpdate(prevProps, prevState) {
-      if (prevState.count !== this.state.count) {
-        localStorage.setItem('count', this.state.count);
+      if (prevState.options.length !== this.state.options.length) {
+        var json = JSON.stringify(this.state.options);
+        localStorage.setItem('options', json);
+        console.log('Saving data.');
       }
     }
   }, {
-    key: 'handleAddOne',
-    value: function handleAddOne() {
+    key: 'componentWillUnmount',
+    value: function componentWillUnmount() {
+      console.log('Component will unmount.');
+    }
+  }, {
+    key: 'handleDeleteOption',
+    value: function handleDeleteOption(optionToRemove) {
       this.setState(function (prevState) {
+        return {
+          options: prevState.options.filter(function (option) {
+            return optionToRemove !== option;
+          })
+        };
+      });
+    }
+  }, {
+    key: 'handleDeleteOptions',
+    value: function handleDeleteOptions() {
+      /*
+      PROBLEM: We want to take advantage of implicit return when using arrow functions, 
+      but the parser treats the first set of braces as the function body. How do we
+      implicitly return an object?
+      
+      SOLUTION: We wrap the object in parentheses causing the interpreter to treat it as 
+      an expression, the result of which is the ojbect, and can be implicitly returned.
+      */
+      this.setState(function () {
+        return { options: [] };
+      });
+    }
+  }, {
+    key: 'handlePick',
+    value: function handlePick() {
+      var randomNum = Math.floor(Math.random() * this.state.options.length);
+      var option = this.state.options[randomNum];
+      alert(option);
+    }
+  }, {
+    key: 'handleAddOption',
+    value: function handleAddOption(option) {
+      if (!option) {
+        return 'Enter valid value to add item.';
+      } else if (this.state.options.indexOf(option) > -1) {
+        return 'This option already exist';
+      }
+
+      this.setState(function (prevState) {
+        return {
+          options: prevState.options.concat(option)
+        };
+      });
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      var subtitle = 'Put your life in the hands of a computer.';
+
+      return React.createElement(
+        'div',
+        null,
+        React.createElement(Header, { subtitle: subtitle }),
+        React.createElement(Action, {
+          hasOptions: this.state.options.length > 0,
+          handlePick: this.handlePick
+        }),
+        React.createElement(Options, {
+          options: this.state.options,
+          handleDeleteOptions: this.handleDeleteOptions,
+          handleDeleteOption: this.handleDeleteOption
+        }),
+        React.createElement(AddOption, {
+          handleAddOption: this.handleAddOption
+        })
+      );
+    }
+  }]);
+
+  return IndecisionApp;
+}(React.Component);
+
+var Header = function Header(props) {
+  return React.createElement(
+    'div',
+    null,
+    React.createElement(
+      'h1',
+      null,
+      props.title
+    ),
+    props.subtitle && React.createElement(
+      'h2',
+      null,
+      props.subtitle
+    )
+  );
+};
+
+Header.defaultProps = {
+  title: 'Indecision'
+};
+
+var Action = function Action(props) {
+  return React.createElement(
+    'div',
+    null,
+    React.createElement(
+      'button',
+      {
+        disabled: !props.hasOptions,
+        onClick: props.handlePick
+      },
+      'What should I do?'
+    )
+  );
+};
+
+var Options = function Options(props) {
+  return React.createElement(
+    'div',
+    null,
+    React.createElement(
+      'button',
+      { onClick: props.handleDeleteOptions },
+      'Remove All'
+    ),
+    props.options.length === 0 && React.createElement(
+      'p',
+      null,
+      'Please add an option to get started.'
+    ),
+
+    /*
+    PROBLEM: We want to break JSX into multiple lines, but retain cleanliness
+    offered by arrow function implicit return.
+      SOLUTION: Use multi-line expressions allow the JSX element to still be 
+    implicitly returned.
+    */
+    props.options.map(function (option) {
+      return React.createElement(Option, {
+        key: option,
+        optionText: option,
+        handleDeleteOption: props.handleDeleteOption
+      });
+    })
+  );
+};
+
+var Option = function Option(props) {
+  return React.createElement(
+    'div',
+    null,
+    props.optionText,
+    React.createElement(
+      'button',
+      {
         /*
-        PROBLEM: When comparing "prevState.count" and "this.state.count" in 
-        componentDidUpdate lifecycle method, logging both variables 
-        revealed the same value for both. As a result, the if condition 
-        never evaluated to true, and the updated value of "count" was never 
-        written to localStorage.
+        PROBLEM: When "Delete" button is clicked, we wnant to pass data to the handler
+        function. But using "onClick={props.handleDeleteOption()}" would call the function
+        at runtime. How do we pass data into the fucntion without running it?
         
-        SOLUTION: Previous implementation used "++" syntax causing unexpected 
-        behaviour due to the fact that ++ and -- are ASSIGNMENT operators, i.e. 
-        they MODIFY the value during evaluation, prior to assignment. As such, 
-        the value of count on prevState was being directly changed before being 
-        stored in the the count property of the object in the setState payload. 
-        This resulted in "prevState.count" and "this.state.count" evaluating to 
-        the same value whenever componentDidUpdate was triggered.
+        SOLUTION: We define a new function to wrap the function we want to call. Within
+        this function we are free to add arguments, as this function will not be called at
+        runtime.
+        
+        PERSPECTIVE: Ensure that you do not think of the data being passed up the chain. 
+        This is not what is happening. What is actually happening is that the function 
+        reference is being passed down to the child element. Also, think of this function
+        wrapping approach as the opposite of an IIFE.
+          ADD'L INFO: When a button element is clicked, the first element passed to a
+        function called in "onClick" is the event object.
         */
-        return { count: prevState.count + 1 };
+        onClick: function onClick(e) {
+          props.handleDeleteOption(props.optionText);
+        }
+      },
+      'Delete'
+    )
+  );
+};
+
+var AddOption = function (_React$Component2) {
+  _inherits(AddOption, _React$Component2);
+
+  function AddOption(props) {
+    _classCallCheck(this, AddOption);
+
+    var _this2 = _possibleConstructorReturn(this, (AddOption.__proto__ || Object.getPrototypeOf(AddOption)).call(this, props));
+
+    _this2.handleAddOption = _this2.handleAddOption.bind(_this2);
+
+    _this2.state = { error: undefined };
+    return _this2;
+  }
+
+  _createClass(AddOption, [{
+    key: 'handleAddOption',
+    value: function handleAddOption(e) {
+      e.preventDefault();
+
+      var option = e.target.elements.option.value.trim();
+      var error = this.props.handleAddOption(option);
+
+      this.setState(function () {
+        return { error: error };
       });
-    }
-  }, {
-    key: 'handleMinusOne',
-    value: function handleMinusOne() {
-      this.setState(function (prevState) {
-        return { count: prevState.count - 1 };
-      });
-    }
-  }, {
-    key: 'handleReset',
-    value: function handleReset() {
-      this.setState(function (prevState) {
-        return { count: 0 };
-      });
+
+      if (!error) {
+        e.target.elements.option.value = '';
+      }
     }
   }, {
     key: 'render',
@@ -87,32 +287,26 @@ var Counter = function (_React$Component) {
       return React.createElement(
         'div',
         null,
-        React.createElement(
-          'h1',
+        this.state.error && React.createElement(
+          'p',
           null,
-          'Count: ',
-          this.state.count
+          this.state.error
         ),
         React.createElement(
-          'button',
-          { onClick: this.handleMinusOne },
-          '-1'
-        ),
-        React.createElement(
-          'button',
-          { onClick: this.handleReset },
-          '0'
-        ),
-        React.createElement(
-          'button',
-          { onClick: this.handleAddOne },
-          '+1'
+          'form',
+          { onSubmit: this.handleAddOption },
+          React.createElement('input', { type: 'text', name: 'option' }),
+          React.createElement(
+            'button',
+            null,
+            'Add Option'
+          )
         )
       );
     }
   }]);
 
-  return Counter;
+  return AddOption;
 }(React.Component);
 
-ReactDOM.render(React.createElement(Counter, null), document.getElementById('app'));
+ReactDOM.render(React.createElement(IndecisionApp, null), document.getElementById('app'));
