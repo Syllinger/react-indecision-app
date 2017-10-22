3b2b46e9c1060f35f5934bff7b8703bbf0595b51
Syllinger <syllinger@gmail.com>
2017-10-21 22:33:18 -0400

    Move ES6 module working files to playground, and install React modules

--- src/app.js
+++ src/app.js
@@ -1,17 +1,4 @@
-/*
-TIP: Because "subtract" is set as a default export, it must be 
-referenced outside of the braces, otherwise a reference error 
-is thrown. It can still be named explicitly, but cannot be 
-within the braces. The point of the default export is that the 
-variable referencing it in the import statement is not required 
-to match that defined in the export statement of the imported 
-file. In this example, the default export is named "subtract" 
-but it is referenced in app.js as "sub."
-*/
-//import './utils';
-import sub, {square, add} from './utils';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-console.log('app.js is running!');
-console.log(square(4));
-console.log(add(2,4));
-console.log(sub(10,2));
+ReactDOM.render(React.createElement('p', {}, 'testing'), document.getElementById('app'));
