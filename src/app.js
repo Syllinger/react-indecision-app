711897c43ee962b48e115301dced3951f4ba516a
Syllinger <syllinger@gmail.com>
2017-10-21 22:43:24 -0400

    Install & config webpack to use babel-core and babel-loader

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(React.createElement('p', {}, 'testing'), document.getElementById('app'));
+ReactDOM.render(<p>This is JSX!</p>, document.getElementById('app'));
