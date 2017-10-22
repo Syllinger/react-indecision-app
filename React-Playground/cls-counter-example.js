177e686033db47c8549c0c622c06f268c98b1763
Syllinger <syllinger@gmail.com>
2017-10-21 20:39:10 -0400

    Update Counter example to persist state in localStorage (iteresting bug)

--- React-Playground/cls-counter-example.js
+++ React-Playground/cls-counter-example.js
@@ -7,19 +7,51 @@ class Counter extends React.Component {
     this.handleReset = this.handleReset.bind(this);
 
     this.state = {
-      count: props.count
+      count: 0
+    }
+  }
+
+  componentDidMount() {
+    try {
+      const count = localStorage.getItem('count');
+      if (count && !isNaN(count)) {
+        this.setState(() => ({count: parseInt(count, 10)}));
+      }
+    } catch(e) {
+
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.count !== this.state.count) {
+      localStorage.setItem('count', this.state.count);
     }
   }
 
   handleAddOne() {
     this.setState(prevState => {
-      return {count: ++prevState.count}
+      /*
+      PROBLEM: When comparing "prevState.count" and "this.state.count" in 
+      componentDidUpdate lifecycle method, logging both variables 
+      revealed the same value for both. As a result, the if condition 
+      never evaluated to true, and the updated value of "count" was never 
+      written to localStorage.
+      
+      SOLUTION: Previous implementation used "++" syntax causing unexpected 
+      behaviour due to the fact that ++ and -- are ASSIGNMENT operators, i.e. 
+      they MODIFY the value during evaluation, prior to assignment. As such, 
+      the value of count on prevState was being directly changed before being 
+      stored in the the count property of the object in the setState payload. 
+      This resulted in "prevState.count" and "this.state.count" evaluating to 
+      the same value whenever componentDidUpdate was triggered.
+      */
+      return {count: prevState.count + 1}
     });
   }
 
   handleMinusOne() {
     this.setState(prevState => {
-      return {count: --prevState.count}
+      return {count: prevState.count - 1}
     });
   }
 
@@ -39,10 +71,6 @@ class Counter extends React.Component {
       </div>
     );
   }
-}
-
-Counter.defaultProps = {
-  count: 0
 }
 
 ReactDOM.render(<Counter />, document.getElementById('app'));
